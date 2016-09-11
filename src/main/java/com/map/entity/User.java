78131c1b9db6bbb68c9e5ee0d0cab3d26343b8f7
Yevhenii Semenov <zhe.semenov@gmail.com>
2016-09-12 00:18:25 +0300

    Initial part of entities (Comment, Location, User) with base fields

---
+++ src/main/java/com/map/entity/User.java
@@ -0,0 +1,67 @@
+package com.map.entity;
+
+/**
+ * @Author Yevhenii Semenov
+ */
+public class User {
+
+    private long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    private String mail;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    /*
+     * Todo: Don't forget about equals and hashcode
+     */
+}
