c0edae68429e6c6668d64e01e3ebb77dafda18c0
Andrew Pasika <and.pasika@gmail.com>
2016-09-17 13:37:16 +0300

    add opportunity to find user by its name & email

--- src/main/java/com/map/repository/UserRepository.java
+++ src/main/java/com/map/repository/UserRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Yevhenii Semenov
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
 }
