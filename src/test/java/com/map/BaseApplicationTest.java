9ad6300a0aaf96daf0c368e6918fce958081200f
Andrew Pasika <and.pasika@gmail.com>
2016-09-18 02:40:37 +0300

    config unit tests

---
+++ src/test/java/com/map/BaseApplicationTest.java
@@ -0,0 +1,14 @@
+package com.map;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Andrew Pasika
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest // TODO: 9/17/16 change it
+public class BaseApplicationTest {
+
+}
