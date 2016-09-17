c1af997d3dd4a901c5187b0572e5207d1d6a60e4
Yevhenii Semenov <zhe.semenov@gmail.com>
2016-09-17 16:28:28 +0300

    Added util package

---
+++ src/main/java/com/map/utils/RestPreconditions.java
@@ -0,0 +1,20 @@
+package com.map.utils;
+
+import com.map.exception.ResourceNotFoundException;
+
+/**
+ * @author Yevhenii Semenov
+ */
+public class RestPreconditions {
+
+    private RestPreconditions() {
+        throw new RuntimeException("Utils class should not have instances");
+    }
+
+    public static <T> T checkFound(final T resource) {
+        if(resource == null) {
+            throw new ResourceNotFoundException();
+        }
+        return resource;
+    }
+}
