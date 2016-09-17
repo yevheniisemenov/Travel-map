151dbf955b2301544754ba164b365033272a0364
Andrew Pasika <apasika@cybervisiontech.com>
2016-09-17 19:38:45 +0300

    add comments and authorities to userDto

--- src/main/java/com/map/dto/UserDto.java
+++ src/main/java/com/map/dto/UserDto.java
@@ -8,6 +8,9 @@ import org.hibernate.validator.constraints.Email;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Yevhenii Semenov, Andrew Pasika
@@ -19,22 +22,18 @@ import javax.validation.constraints.Size;
 public class UserDto {
 
     private Long id;
-
     @Size(min = 2, max = 24)
     private String name;
-
     @Size(min = 2, max = 24)
     private String surname;
-
     @NotNull
     @Size(min = 2, max = 24)
     private String username;
-
     @Size(min = 6)
     private String password;
-
     @NotNull
     @Email
     private String email;
-
+    private List<CommentDto> comments = new ArrayList<>();
+    private Collection<RoleDto> authorities = new ArrayList<>();
 }
