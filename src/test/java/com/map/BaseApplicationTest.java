fe01fcf1d50a0d28e0c3b32207e6b5beadc6fb6e
Andrew Pasika <and.pasika@gmail.com>
2016-09-28 00:12:19 +0300

    add ObjectMapper to baseTest

--- src/test/java/com/map/BaseApplicationTest.java
+++ src/test/java/com/map/BaseApplicationTest.java
@@ -1,6 +1,9 @@
 package com.map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,4 +14,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest // TODO: 9/17/16 change it
 public class BaseApplicationTest {
 
+    protected ObjectMapper objectMapper;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        objectMapper = new ObjectMapper();
+    }
 }
