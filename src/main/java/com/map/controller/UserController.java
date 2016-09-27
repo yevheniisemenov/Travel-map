93582b27261ed34f37bb788b0629c1b02f26ae9e
Andrew Pasika <and.pasika@gmail.com>
2016-09-28 00:04:25 +0300

    integration with UserDto & UserEntity converters

--- src/main/java/com/map/controller/UserController.java
+++ src/main/java/com/map/controller/UserController.java
@@ -4,10 +4,10 @@ import com.map.config.security.SecurityAuthService;
 import com.map.dto.UserDto;
 import com.map.model.User;
 import com.map.service.UserService;
-import org.modelmapper.ModelMapper;
+import com.map.util.converter.dto.UserDtoConverter;
+import com.map.util.converter.entity.UserEntityConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -19,7 +19,8 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
- * @author Yevhenii Semenov, Andrew Pasika
+ * @author Andrew Pasika
+ * @author Yevhenii Semenov
  */
 @Controller
 @RequestMapping(USER_BASE_PATH)
@@ -32,13 +33,16 @@ public class UserController {
     SecurityAuthService authService;
 
     @Autowired
-    ModelMapper modelMapper;
+    UserDtoConverter userDtoConverter;
+
+    @Autowired
+    UserEntityConverter userEntityConverter;
 
     @ResponseStatus(CREATED)
     @RequestMapping(path = "/register", method = POST, consumes = MULTIPART_FORM_DATA_VALUE)
-    public UserDto register(@Valid UserDto user, Errors errors) {
-        User registeredUser = userService.register(modelMapper.map(user, User.class), errors);
+    public UserDto register(@Valid UserDto userDto) {
+        User registeredUser = userService.register(userEntityConverter.convert(userDto));
         authService.autoLogin(registeredUser);
-        return modelMapper.map(registeredUser, UserDto.class);
+        return userDtoConverter.convert(registeredUser);
     }
 }
