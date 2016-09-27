3df6958373ef30831ec971ac00690f63cc4c62d3
Andrew Pasika <and.pasika@gmail.com>
2016-09-28 00:09:50 +0300

    remove @Service from UserService interface

--- src/main/java/com/map/service/UserService.java
+++ src/main/java/com/map/service/UserService.java
@@ -1,13 +1,11 @@
 package com.map.service;
 
 import com.map.model.User;
-import org.springframework.stereotype.Service;
 
 /**
  * @author Andrew Pasika
  * @author Yevhenii Semenov
  */
-@Service
 public interface UserService {
 
     User register(User user);
